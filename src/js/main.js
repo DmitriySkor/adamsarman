bd8b4eee5b38223e507d8bd59e6e42c47305c725
DmitriySkor <rionskey@gmail.com>
2020-10-26 12:59:20 +0200

    add range slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,18 @@
+$(function () {
+    $("#slider-range").slider({
+        range: true,
+        min: 0,
+        max: 10000,
+        values: [1250, 5500],
+        slide: function (event, ui) {
+            $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
+        }
+    });
+    $("#amount").val("$" + $("#slider-range").slider("values", 0) +
+        " - $" + $("#slider-range").slider("values", 1));
+});
+
+
 // Инициализация owlCarousel
 $(document).ready(function () {
     $(".owl-carousel").owlCarousel();
@@ -181,17 +196,4 @@ $(function () {
     });
 });
 
-$(function () {
-    $("#slider-range").slider({
-        range: true,
-        min: 0,
-        max: 10000,
-        values: [1250, 5500],
-        slide: function (event, ui) {
-            $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
-        }
-    });
-    $("#amount").val("$" + $("#slider-range").slider("values", 0) +
-        " - $" + $("#slider-range").slider("values", 1));
-});
 
